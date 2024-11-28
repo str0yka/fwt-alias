6428fec48cb3d7779486d3142531628b69c41823
d.kozlov <d.kozlov@framework.team>
2024-11-28 12:59:16 +0400

    feat: Сделал header, добавил иконку лог

--- src/widgets/layout/ui/layout-header.tsx
+++ src/widgets/layout/ui/layout-header.tsx
@@ -1,1 +1,19 @@
-export const LayoutHeader = () => null;
+import { Box } from '@mui/material';
+
+import { ReactComponent as Logo } from '@app/assets/icons/logo.svg';
+
+export const LayoutHeader = () => (
+  <header>
+    <Box
+      sx={{
+        borderBottom: '1px solid #18181B',
+        padding: '25px 0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Logo />
+    </Box>
+  </header>
+);
