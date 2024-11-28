77202bfc4c3188523f982a370fd84240947fe82e
d.kozlov <d.kozlov@framework.team>
2024-11-28 13:07:14 +0400

    feat: сверстал главную страницу

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,8 +1,31 @@
+import { Box } from '@mui/material';
+
 import { Button } from '@shared/ui';
 
 export const HomePage = () => (
   <>
-    <Button variant='contained'>Создать игру</Button>
-    <Button variant='outlined'>Создать игру</Button>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        flexGrow: '1',
+        gap: '10px 0'
+      }}
+    >
+      <Button
+        sx={{ width: '100%' }}
+        variant='contained'
+      >
+        Создать игру
+      </Button>
+      <Button
+        sx={{ width: '100%' }}
+        variant='outlined'
+      >
+        Создать игру
+      </Button>
+    </Box>
   </>
 );
--- src/widgets/layout/ui/layout.tsx
+++ src/widgets/layout/ui/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { LayoutHeader } from './layout-header';
 
 export const Layout = () => (
-  <main>
+  <main style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', padding: '0 12px' }}>
     <LayoutHeader />
     <Outlet />
   </main>
